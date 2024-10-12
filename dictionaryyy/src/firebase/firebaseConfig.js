00662efbd7e355ce4f231a1956ac8cf583ea9d53
edberisha <ed@peak.watch>
2024-10-12 18:18:57 -0400

    added some console logs to see if firebase keys are there

--- dictionaryyy/src/firebase/firebaseConfig.js
+++ dictionaryyy/src/firebase/firebaseConfig.js
@@ -1,16 +1,24 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+
+
+
 // Check if an app is already initialized
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  
 };
+
+console.log("Firebase API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY ? "Defined" : "Not defined");
+console.log("Auth Domain:", process.env.NEXT_PUBLIC_AUTH_DOMAIN ? "Defined" : "Not defined");
 
 // Initialize Firebase app only if it hasn't been initialized yet
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
